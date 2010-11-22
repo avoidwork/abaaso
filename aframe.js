4fea783aa63bcc3366dfccc211309bcb07b7c808
Jason Mulligan <jason.mulligan@avoidwork.com>
2010-11-22 07:33:51 -0500

    Revised the comment block at the top of the library, adding the BSD3 license back to the file.

--- aframe.js
+++ aframe.js
@@ -1,22 +1,42 @@
 /**
+ * Copyright (c) 2010, avoidwork inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL avoidwork inc. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
  * aFrame
  *
  * "An A-frame is a basic structure designed to bear a load in a lightweight economical manner."
  *
- * aFrame provides a set of classes and object prototyping to ease the creation and maintenance of RESTful JavaScript web applications.
+ * aFrame provides a set of classes and object prototyping to ease the creation and maintenance of RESTful JavaScript applications.
  *
  * @author Jason Mulligan <jason.mulligan@avoidwork.com>
- * @copyright Copyright (c) 2010, avoidwork inc.
- * @license http://en.wikipedia.org/wiki/BSD_licenses#3-clause_license_.28.22New_BSD_License.22.29
- * @link http://avoidwork.com/products/aframe aFrame
- * @version Alpha
- */
-
-/**
- * aFrame JavaScript framework
- *
  * @class
+ * @link http://avoidwork.com/products/aframe aFrame
  * @namespace
+ * @version Alpha
  */
 var aFrame = function(){
 	/**
