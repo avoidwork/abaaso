88c588a1d88102b686c51a172d1be73a36d6b824
Jason Mulligan <jason.mulligan@avoidwork.com>
2011-02-26 09:35:44 -0500

    Removed label.months{}, should be in abaaso.calendar module

--- abaaso.js
+++ abaaso.js
@@ -1357,24 +1357,6 @@ var abaaso = function(){
 			invalidFields         : "The following required fields are invalid: ",
 			serverError           : "A server error has occurred.",
 			serverUnauthorized    : "Unauthorized to access URI."
-		},
-
-		/**
-		 * Months
-		 */
-		months : {
-			"1"  : "January",
-			"2"  : "February",
-			"3"  : "March",
-			"4"  : "April",
-			"5"  : "May",
-			"6"  : "June",
-			"7"  : "July",
-			"8"  : "August",
-			"9"  : "September",
-			"10" : "October",
-			"11" : "November",
-			"12" : "December"
 		}
 	};
 
