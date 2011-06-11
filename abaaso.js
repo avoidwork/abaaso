5457968fb83b2eedf114b3dd5330969f6182308d
Jason Mulligan <jason.mulligan@avoidwork.com>
2011-06-11 18:42:35 -0400

    Revert "Forgot the string portion in the last commit"
    This reverts commit e70add5f54f5fb2c03c3efd11e164a1ffe407d70.

--- abaaso.js
+++ abaaso.js
@@ -2582,7 +2582,7 @@ var abaaso = abaaso || function(){
 			try {
 				if ((typeof obj != "undefined")
 					&& ((obj instanceof Array)
-						|| (typeof obj == "string")
+						|| (obj instanceof String)
 						|| ((typeof obj.id != "undefined")
 							&& (obj.id != "")))) {
 					return obj;
@@ -2898,21 +2898,21 @@ var abaaso = abaaso || function(){
 							((typeof this == "object")
 							 && ((this.id === undefined)
 							     || (this.id.isEmpty()))) ? this.genId() : void(0);
-							(typeof this == "string") ? (this.constructor = new String("")) : abaaso.clear(this);
+							(this instanceof String) ? (this.constructor = new String("")) : abaaso.clear(this);
 							return this;
 							}},
 						{name: "destroy", fn: function() {
 							abaaso.destroy(this);
 							}},
 						{name: "domId", fn: function() {
-							if (typeof this != "string") {
+							if (!this instanceof String) {
 								this.genId();
 								return abaaso.domId(this.id);
 							}
 							return abaaso.domId(this);
 							}},
 						{name: "fire", fn: function(event, args) {
-							((typeof this != "string")
+							((!this instanceof String)
 								 && ((this.id === undefined)
 								     || (this.id.isEmpty()))) ? this.genId() : void(0);
 							return abaaso.fire.call(this, event, args);
@@ -2921,13 +2921,13 @@ var abaaso = abaaso || function(){
 							return abaaso.genId(this);
 							}},
 						{name: "listeners", fn: function(event) {
-							((typeof this != "string")
+							((!this instanceof String)
 								 && ((this.id === undefined)
 								     || (this.id.isEmpty()))) ? this.genId() : void(0);
 							return abaaso.listeners(this, event);
 							}},
 						{name: "un", fn: function(event, id) {
-							((typeof this != "string")
+							((!this instanceof String)
 								 && ((this.id === undefined)
 								     || (this.id.isEmpty()))) ? this.genId() : void(0);
 							return abaaso.un(this, event, id);
