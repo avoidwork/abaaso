8efaa22801d0993cb5874651f0c14d239a2502a8
Jason Mulligan <jason.mulligan@avoidwork.com>
2011-11-28 09:58:33 -0500

    Had a ternary backwards in $()

--- abaaso.js
+++ abaaso.js
@@ -42,7 +42,7 @@
  * @author Jason Mulligan <jason.mulligan@avoidwork.com>
  * @link http://abaaso.com/
  * @module abaaso
- * @version 1.7.64
+ * @version 1.7.65
  */
  var $ = $ || null, abaaso = (function() {
 	"use strict";
@@ -2701,7 +2701,7 @@
 					obj = document[sel.indexOf("#") > -1 && sel.indexOf(":") === -1 ? "querySelector" : "querySelectorAll"](arg);
 					break;
 				case arg.indexOf("#") === 0 && arg.indexOf(":") === -1:
-					obj = !isNaN(arg.slice(1)) ? document.getElementById(arg.substring(1)) : document.querySelector(arg);
+					obj = !isNaN(arg.slice(1)) ? document.querySelector(arg) : document.getElementById(arg.substring(1));
 					break;
 				case arg.indexOf("#") > -1 && arg.indexOf(":") === -1:
 					obj = document.querySelector(arg);
@@ -3854,7 +3854,7 @@
 			return observer.remove.call(observer, obj, event, id);
 		},
 		update          : el.update,
-		version         : "1.7.64"
+		version         : "1.7.65"
 	};
 })();
 if (typeof abaaso.bootstrap === "function") abaaso.bootstrap();
