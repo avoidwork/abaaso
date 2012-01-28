9d2a7d5b8339a8d70d8a551ea952b30b79573013
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-01-28 10:06:21 -0500

    Updated copyright in license

--- abaaso.js
+++ abaaso.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2011, Jason Mulligan <jason.mulligan@avoidwork.com>
+ * Copyright (c) 2010 - 2012, Jason Mulligan <jason.mulligan@avoidwork.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
