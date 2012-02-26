30271ae682e963d847d11325f3ea832f0ae5878b
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-02-26 13:21:56 -0500

    Updated client.request() to decode JSON returned within JSONP responses (Flickr, etc.)

--- debug/abaaso.js
+++ debug/abaaso.js
@@ -42,7 +42,7 @@
  * @author Jason Mulligan <jason.mulligan@avoidwork.com>
  * @link http://abaaso.com/
  * @module abaaso
- * @version 1.9.1
+ * @version 1.9.2
  */
 (function (window) {
 
@@ -4393,7 +4393,7 @@ if (typeof window.abaaso === "undefined") window.abaaso = (function () {
 			return observer.remove.call(observer, o, e, i, s);
 		},
 		update          : el.update,
-		version         : "1.9.1"
+		version         : "1.9.2"
 	};
 })();
 if (typeof abaaso.bootstrap === "function") abaaso.bootstrap();
