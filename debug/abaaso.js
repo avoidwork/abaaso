9f6234ed15570f501dad25cd5c6b796faaecbecd
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-06-25 08:45:08 -0400

    Created $.stylesheet() for fast injection of CSS assets

--- debug/abaaso.js
+++ debug/abaaso.js
@@ -4719,6 +4719,7 @@ if (typeof global.abaaso === "undefined") global.abaaso = (function () {
 		reflect         : utility.reflect,
 		repeat          : utility.repeat,
 		repeating       : {},
+		stylesheet      : function (arg, media) { return element.create("link", {rel: "stylesheet", type: "text/css", src: arg, media: media || "screen, print"}, $("head")[0]); },
 		script          : function (arg, target, pos) { return element.create("script", {type: "application/javascript", src: arg}, target || $("head")[0], pos); },
 		store           : function (arg, args) { return data.register.call(data, arg, args); },
 		timer           : {},
