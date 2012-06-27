35a290fa7bf2cd5b4b01f07a31560b817f0e3073
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-06-26 20:42:52 -0400

    Fixing data.crawl() for optional parameters

--- debug/abaaso.js
+++ debug/abaaso.js
@@ -1244,17 +1244,22 @@ if (typeof global.abaaso === "undefined") global.abaaso = (function () {
 			 * @return {Object}        Record
 			 */
 			crawl : function (arg, ignore, key) {
-				var record;
+				var ignored = false,
+				    record;
 
 				if (typeof arg === "undefined" || (typeof arg !== "number" && typeof arg !== "string")) throw Error(label.error.invalidArguments);
 
 				record = this.get(arg);
 				record = this.records[this.keys[record.key].index];
 				key    = key || this.key;
-				ignore = ignore.explode() || [];
+
+				if (typeof ignore === "string") {
+					ignored = true;
+					ignore  = ignore.explode();
+				}
 
 				utility.iterate(record.data, function (v, k) {
-					if (ignore.index(k) > -1) return;
+					if (ignored && ignore.index(k) > -1) return;
 					if (v.indexOf("//") >= 0) {
 						record.data[k] = data.register({id: record.key + "-" + k});
 						record.data[k].data.key = key;
