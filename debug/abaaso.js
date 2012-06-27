55abfddf0d35fa55f86cbdf5b02eb7c097e67a77
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-06-27 13:54:42 -0400

    Version bump

--- debug/abaaso.js
+++ debug/abaaso.js
@@ -44,7 +44,7 @@
  * @author Jason Mulligan <jason.mulligan@avoidwork.com>
  * @link http://abaaso.com/
  * @module abaaso
- * @version 2.3.7
+ * @version 2.3.8
  */
 (function (global) {
 "use strict";
@@ -4822,7 +4822,7 @@ if (typeof global.abaaso === "undefined") global.abaaso = (function () {
 			return observer.remove.call(observer, o, e, i, s);
 		},
 		update          : element.update,
-		version         : "2.3.7"
+		version         : "2.3.8"
 	};
 })();
 if (typeof abaaso.bootstrap === "function") abaaso.bootstrap();
