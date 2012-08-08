7f6fbf665eaa78ddaa0d972251e62319eee29716
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-08-08 11:39:13 -0400

    Created json unit tests

--- test/json.js
+++ test/json.js
@@ -1,1 +1,26 @@
 var json = require("../lib/abaaso.js").abaaso.json;
+
+exports["decode"] = {
+	setUp: function (done) {
+		this.val = "{\"abc\":true}";
+		done();
+	},
+	direct: function (test) {
+		test.expect(2);
+		test.equal(json.decode(this.val) instanceof Object, true, "Should be true");
+		test.equal(json.decode(this.val)["abc"], true, "Should be true");
+		test.done();
+	}
+};
+
+exports["encode"] = {
+	setUp: function (done) {
+		this.val = {abc:true};
+		done();
+	},
+	direct: function (test) {
+		test.expect(1);
+		test.equal(typeof json.encode(this.val) === "string", true, "Should be true");
+		test.done();
+	}
+};
