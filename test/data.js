54861e8d28455b8f56f1f5f7c9284ed9d1e9e9b9
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-08-08 16:41:07 -0400

    Adding 2 more assertions to a data unit test, version bump

--- test/data.js
+++ test/data.js
@@ -25,9 +25,11 @@ exports["batch"] = {
 		done();
 	},
 	direct: function (test) {
-		test.expect(2);
+		test.expect(4);
 		test.equal(this.store.data.batch("set", sample), this.store.data, "Should match");
+		test.equal(this.store.data.total, 2, "Should be 2");
 		test.equal(this.store.data.batch("del", [0, 1]), this.store.data, "Should match");
+		test.equal(this.store.data.total, 0, "Should be 0");
 		test.done();
 	}
 };
