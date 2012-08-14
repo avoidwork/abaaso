f7b0da719a686fe0514c8efda270dbcbf8d9c485
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-08-14 06:52:44 -0400

    Early morning continuation of refactoring; might not survive after coffee

--- src/route.js
+++ src/route.js
@@ -11,11 +11,27 @@ var route = {
 
 	// Routing listeners
 	routes : {
-		error : function () {
-			utility.error(label.error.invalidArguments);
-			if (abaaso.route.initial !== null) route.hash(abaaso.route.initial);
-		}
+		all   : {
+			error : function () {
+				utility.error(label.error.invalidArguments);
+				if (abaaso.route.initial !== null) route.hash(abaaso.route.initial);
+			}
+		},
+		del   : {},
+		get   : {},
+		put   : {},
+		post  : {}
 	},
+
+	/**
+	 * Determines which HTTP method to use
+	 * 
+	 * @param  {String} arg HTTP method
+	 * @return {[type]}     HTTP method to utilize
+	 */
+	method = function (arg) {
+		return /all|del|get|put|post/gi.test(arg) ? arg.toLowerCase() : "all";
+	};
 
 	/**
 	 * Deletes a route
@@ -77,15 +93,18 @@ var route = {
 	 * Loads the hash into the view
 	 * 
 	 * @method load
-	 * @param  {String} name Route to load
-	 * @return {Mixed}       True or undefined
+	 * @param  {String} name  Route to load
+	 * @param  {Object} arg   HTTP response (node)
+	 * @param  {String} verb  HTTP method
+	 * @return {Mixed}        True or undefined
 	 */
-	load : function (name, arg) {
+	load : function (name, arg, verb) {
+		verb = method(verb);
 		var active = "error";
 
 		name = name.replace(route.bang, "");
-		if (typeof route.routes[name] !== "undefined") active = name;
-		else utility.iterate(route.routes, function (v, k) { if (utility.compile(route.regex, "^" + k + "$", "i") && route.regex.test(name)) return !(active = k); });
+		if (typeof route.routes[verb][name] !== "undefined") active = name;
+		else utility.iterate(route.routes[verb], function (v, k) { if (utility.compile(route.regex, "^" + k + "$", "i") && route.regex.test(name)) return !(active = k); });
 		route.routes[active](arg || name);
 		return true;
 	},
@@ -93,8 +112,9 @@ var route = {
 	/**
 	 * Creates a Server with URI routing
 	 * 
-	 * @param  {Object} arg Server options
-	 * @return {Undefined}  undefined
+	 * @method server
+	 * @param  {Object} arg  Server options
+	 * @return {fn}     fn   Error handler
 	 * @todo  Add SSL support
 	 */
 	server : function (args, fn) {
@@ -109,7 +129,7 @@ var route = {
 		args.verb = args.verb           || "GET";
 
 		http.createServer(function (req, res) {
-			if (req.method === args.verb) route.load(req.url, res);
+			route.load(req.url, res, req.method);
 		}).on("error", function (e) {
 			error(e, this, arguments);
 			if (typeof fn === "function") fn(e);
@@ -120,14 +140,15 @@ var route = {
 	 * Sets a route for a URI
 	 * 
 	 * @method set
-	 * @param  {String}   name Regex pattern for the route
-	 * @param  {Function} fn   Route listener
-	 * @return {Mixed}         True or undefined
+	 * @param  {String}   name  Regex pattern for the route
+	 * @param  {Function} fn    Route listener
+	 * @param  {String}   verb  HTTP method the route is for (default is GET)
+	 * @return {Mixed}          True or undefined
 	 */
-	set : function (name, fn) {
+	set : function (name, fn, verb) {
 		if (typeof name !== "string" || name.isEmpty() || typeof fn !== "function") throw Error(label.error.invalidArguments);
 
-		route.routes[name] = fn;
+		route.routes[method(verb)][name] = fn;
 		return true;
 	}
 };
