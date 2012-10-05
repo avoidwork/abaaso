840b48416eec964bceb862973662cfb37d6a785f
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-10-05 18:49:19 -0400

    Minor syntax change: refactored data.del() -> data.delete() & String.prototype.del -> String.prototype.delete()

--- src/data.js
+++ src/data.js
@@ -111,7 +111,7 @@ var data = {
 									i.get(function (arg) { set(self.source === null ? arg : utility.walk(arg, self.source), idx); }, failure, utility.merge({withCredentials: self.credentials}, self.headers));
 									break;
 							}
-							else self.del(i, false, sync);
+							else self.delete(i, false, sync);
 						});
 				}
 			}
@@ -126,7 +126,7 @@ var data = {
 						else key = k.toString();
 						self.set(key, v, sync);
 					}
-					else self.del(v, false, sync);
+					else self.delete(v, false, sync);
 				});
 			}
 
@@ -268,7 +268,7 @@ var data = {
 		 * @param  {Boolean} sync    [Optional] True if called by data.sync
 		 * @return {Object}          Data store
 		 */
-		del : function (record, reindex, sync) {
+		delete : function (record, reindex, sync) {
 			if (typeof record === "undefined" || !/number|string/.test(typeof record)) throw Error(label.error.invalidArguments);
 
 			reindex  = (reindex !== false);
@@ -305,7 +305,7 @@ var data = {
 					obj.fire("syncDataDelete", args);
 					break;
 				case r.test(p):
-					uri.del(function () { obj.fire("syncDataDelete", args); }, function () { obj.fire("failedDataDelete", args); }, utility.merge({withCredentials: this.credentials}, this.headers));
+					uri.delete(function () { obj.fire("syncDataDelete", args); }, function () { obj.fire("failedDataDelete", args); }, utility.merge({withCredentials: this.credentials}, this.headers));
 					break;
 				default:
 					obj.fire("failedDataDelete", args);
