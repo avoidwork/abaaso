9bb27fabf1fd5cb44f54b0878a361d37efb9aa52
Jason Mulligan <jason.mulligan@avoidwork.com>
2012-10-14 17:31:47 -0400

    Re-enabled filter() in build

--- grunt.js
+++ grunt.js
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
 					"src/data.js",
 					"src/datalist.js",
 					"src/element.js",
-					//"src/filter.js",
+					"src/filter.js",
 					"src/json.js",
 					"src/label.js",
 					"src/message.js",
