6ac2cbc1b210a042e495059eeafdac0135c0b7fa
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-08-25 14:01:17 -0400

    Minor tweak to `benchmark.js`

--- benchmark.js
+++ benchmark.js
@@ -1,28 +1,24 @@
 // Inspired by http://swannodette.github.io/2013/08/23/make-no-promises/
-function go() {
-	var $     = require("./lib/abaaso"),
-	    first = $.defer(),
-	    last  = first.promise,
-	    nth   = 100000,
-	    i     = -1, s;
+var $     = require("./lib/abaaso"),
+    first = $.defer(),
+    last  = first.promise,
+    nth   = 100000,
+    i     = -1, s;
 
-	while ( ++i < nth ) {
-		last = last.then(function(val) {
-			return val + 1;
-		});
-	}
-
-	last.then (function ( val ) {
-		var ms = new Date() - s;
-
-		console.log( "Completed " + val.format() + " in " + ms.format() + "ms" );
-		process.exit(0);
+while ( ++i < nth ) {
+	last = last.then(function(val) {
+		return val + 1;
 	});
-
-	console.log( "Promises/A+ benchmark - resolving " + nth.format() + " promises" );
-
-	s = new Date();
-	first.resolve(0);
 }
 
-go();
+last.then (function ( val ) {
+	var ms = new Date() - s;
+
+	console.log( "Completed " + val.format() + " in " + ms.format() + "ms" );
+	process.exit(0);
+});
+
+console.log( "Promises/A+ benchmark - resolving " + nth.format() + " promises" );
+
+s = new Date();
+first.resolve(0);
