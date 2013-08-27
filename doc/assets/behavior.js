e8ef9d56fc26b1b58bacf892089f8a33828f2f74
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-08-27 17:39:50 -0400

    Updating `DataStore.sort()` benchmark and docs

--- doc/assets/behavior.js
+++ doc/assets/behavior.js
@@ -560,24 +560,6 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "ignorePrefix": "}",
           "name": "JavaScript"
         },
-        "sourcePath": "/Users/jason/Projects/abaaso/src/route.js",
-        "projectPath": "src/route.js",
-        "targetPath": "route",
-        "title": "route"
-      },
-      "depth": 1,
-      "outline": []
-    }, {
-      "type": "file",
-      "data": {
-        "language": {
-          "nameMatchers": [".js"],
-          "pygmentsLexer": "javascript",
-          "singleLineComment": ["//"],
-          "multiLineComment": ["/*", "*", "*/"],
-          "ignorePrefix": "}",
-          "name": "JavaScript"
-        },
         "sourcePath": "/Users/jason/Projects/abaaso/src/state.js",
         "projectPath": "src/state.js",
         "targetPath": "state",
